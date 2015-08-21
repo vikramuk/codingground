2275cc7de80835d645bae28ff1128dedc27ee4c9
vikramuk <vikramuk@gmail.com>
2015-08-21 16:28:37 +0530

    Created by Coding Ground Client

---
+++ New Project/Unittest3.py
@@ -0,0 +1,12 @@
+import unittest
+import Test1
+import Test2
+
+test_cases = (Test1, Test2)
+
+def load_tests(loader, tests, pattern):
+    suite = unittest.TestSuite()
+    for test_class in test_cases:
+        tests = loader.loadTestsFromTestCase(test_class)
+        suite.addTests(tests)
+    return suite
