aa9e77c968abb60a8968987757226510d67a42e6
vikramuk <vikramuk@gmail.com>
2015-08-21 16:28:36 +0530

    Created by Coding Ground Client

---
+++ New Project/TestClassC.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestClassC(unittest.TestCase):
+    def testOne(self):
+        # test code
+        pass
+    
+if __name__ == '__main__':
+    unittest.main(verbosity=1)
+    print "completed TestC"    
