fd635fdaacb69df6902187418e788f52b4f9e1b1
vikramuk <vikramuk@gmail.com>
2015-08-24 14:41:00 +0530

    Created by Coding Ground Client

---
+++ SeleniumPython/UnitTest_Demo2.py
@@ -0,0 +1,40 @@
+import unittest
+from selenium import webdriver
+
+
+class SearchTest2(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Firefox()
+        cls.driver.implicitly_wait(20)
+        cls.driver.maximize_window()
+        cls.driver.get("http://demo.magentocommerce.com")
+
+    def test_Phones(self):
+        self.search_field = self.driver.find_element_by_name("q")
+        self.search_field.send_keys("Phones")
+        self.search_field.submit()
+        self.button = self.driver.find_element_by_xpath(".//*[@id='search_mini_form']/div[1]/button")
+        self.button.click()
+        self.products = self.driver.find_elements_by_css_selector("h2.product-name")
+        self.assertEqual(len(self.products), 3)
+        print(len(self.products))
+        
+
+    def test_Pants(self):
+        self.search_field = self.driver.find_element_by_name("q")
+        self.search_field.send_keys("Pant")
+        self.search_field.submit()
+        self.button = self.driver.find_element_by_xpath(".//*[@id='search_mini_form']/div[1]/button")
+        self.button.click()
+        self.products = self.driver.find_elements_by_css_selector("h2.product-name")
+        self.assertEqual(len(self.products), 4)
+        print(len(self.products))
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
