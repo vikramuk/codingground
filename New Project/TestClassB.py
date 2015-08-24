e4e733b0b5f32ee1ad34612090a5160219db356b
vikramuk <vikramuk@gmail.com>
2015-08-24 12:05:29 +0530

    Created by Coding Ground Client

---
+++ New Project/TestClassB.py
@@ -0,0 +1,19 @@
+import unittest
+
+class TestClassB(unittest.TestCase):
+    def testtwo(self):
+        # test code
+        pass
+    
+    
+    def testBtwo(self):
+        # test code
+        pass
+    
+    def testBthree(self):
+        # test code
+        pass
+    
+if __name__ == '__main__':
+    unittest.main(verbosity=1)
+    print "completed TestB"   
