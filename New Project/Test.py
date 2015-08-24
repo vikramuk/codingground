1d5728fb48ef7239b060f0e91eb9c63c18826565
vikramuk <vikramuk@gmail.com>
2015-08-24 12:05:28 +0530

    Created by Coding Ground Client

---
+++ New Project/Test.py
@@ -0,0 +1,21 @@
+print "Testcase2"
+import unittest
+
+class TestMe(unittest.TestCase):
+
+    def testFailUnless(self):
+        self.failUnless(True)
+
+    def testAssertTrue(self):
+        self.assertTrue(True)
+
+    def testFailIf(self):
+        self.failIf(False)
+
+    def testAssertFalse(self):
+        self.assertFalse(False)
+
+if __name__ == '__main__':
+    unittest.main()
+    print "Inside Test2"
+
