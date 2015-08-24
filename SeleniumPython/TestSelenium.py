3e8f5df34cb5f34f656cacdcbddebad19cf895d4
vikramuk <vikramuk@gmail.com>
2015-08-24 13:04:07 +0530

    Created by Coding Ground Client

---
+++ SeleniumPython/TestSelenium.py
@@ -0,0 +1,32 @@
+# -*- coding: cp1252 -*-
+from selenium import selenium
+from HtmlTestRun import HTMLTestRunner
+import unittest, time
+'''
+https://dhirendrajha.wordpress.com/2011/08/31/html-report-using-htmltestrunner/
+'''
+
+class main_script(unittest.TestCase):
+    pass
+#   selenium = selenium("localhost", 4444, "*chrome","http://google.co.in")
+#   selenium.start()
+#   selenium.open("/")
+
+
+def test01_script(self):
+    pass
+#   browser=self.selenium
+#   print "Open the google page"
+#   browser.type("q", "selenium RC")
+#   print "Enter the search test Selenium RC"
+#   browser.click("btnG")
+#   print "Click on Google Search button"
+#   browser.stop()
+
+if __name__ == '__main__':
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(main_script))
+    dateTimeStamp = time.strftime('%Y%m%d_%H_%M_%S')
+    buf = file("TestReport" + "_" + dateTimeStamp + ".html", 'wb')
+    runner = HTMLTestRunner(stream=buf,title='UnitTest HTML Report',description='Result of tests')
+    runner.run(suite)
